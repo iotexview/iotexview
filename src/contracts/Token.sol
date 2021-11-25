b10d5b88345936dc2d6941f063b43609022863c2
matifalcone <matias.falcone@gmail.com>
2021-11-25 17:13:06 +1100

    Token Pausable

--- src/contracts/Token.sol
+++ src/contracts/Token.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Pausable.sol";
 
-contract IotexViewToken is ERC20 {
+contract IotexViewToken is ERC20Pausable {
   constructor(string memory _name, string memory _symbol) 
     ERC20(_name, _symbol)
   {
